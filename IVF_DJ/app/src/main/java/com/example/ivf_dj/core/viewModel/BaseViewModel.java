51b0e4194915b9ed20384897a75a88b0b4ae4fa2
SungChulByun <sungchul.byun@navercorp.com>
2020-09-02 00:23:37 +0900

    0902(수) 00:23 뷰모델 세팅

--- IVF_DJ/app/src/main/java/com/example/ivf_dj/core/viewModel/BaseViewModel.java
+++ IVF_DJ/app/src/main/java/com/example/ivf_dj/core/viewModel/BaseViewModel.java
@@ -2,8 +2,6 @@ package com.example.ivf_dj.core.viewModel;
 
 import androidx.lifecycle.ViewModel;
 
-import io.reactivex.disposables.CompositeDisposable;
-
 public class BaseViewModel extends ViewModel {
 
 }
--- IVF_DJ/app/src/main/java/com/example/ivf_dj/feature/login/LoginActivity.java
+++ IVF_DJ/app/src/main/java/com/example/ivf_dj/feature/login/LoginActivity.java
@@ -1,16 +1,34 @@
 package com.example.ivf_dj.feature.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.example.ivf_dj.R;
+import com.example.ivf_dj.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding mBinding;
+    private LoginViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        initViewModel();
+        initViews();
+    }
+
+    private void initViewModel(){
+        mViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        mBinding.setViewModel(mViewModel);
+    }
+
+    private void initViews(){
+
     }
 }
